80718feaaabab9e33dc58c181896292631da1326
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2026-02-02 13:11:11 +0100

    ballenbak

---
+++ Testing Stuff 2D/Assets/Ballenbak/Scripts/SpawnBallsOnTimer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnBallsOnTimer : MonoBehaviour
+{
+    public GameObject ball;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(SpawnBall());
+    }
+
+    IEnumerator SpawnBall()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            GameObject lastBall = Instantiate(ball, new Vector3(transform.position.x + Random.Range(-5, 5), transform.position.y, transform.position.z), Quaternion.identity);
+            SpriteRenderer lBSR = lastBall.GetComponent<SpriteRenderer>();
+            lBSR.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            Rigidbody2D rb = lastBall.GetComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2(Random.Range(-5, 5), Random.Range(-5, 5)));
+        }
+    }
+}
--- Testing Stuff 2D/Assets/General/Scripts/PlayerMovement.cs
+++ Testing Stuff 2D/Assets/General/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 input;
+    bool grounded = false;
+    float jumpHeight = 5;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,5 +20,29 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext c)
     {
         input = c.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && grounded == true)
+        {
+            rb.AddForce(new Vector2(0, 1 * jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            grounded = false;
+        }
     }
 }
