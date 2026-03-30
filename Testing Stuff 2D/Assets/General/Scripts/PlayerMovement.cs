6c846f49a3b439cc40e82edaf278341e94aef178
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2026-03-30 13:27:42 +0200

    art

--- Testing Stuff 2D/Assets/General/Scripts/PlayerMovement.cs
+++ Testing Stuff 2D/Assets/General/Scripts/PlayerMovement.cs
@@ -5,17 +5,21 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 input;
+    Animator animator;
+    SpriteRenderer sr;
 
     public float speed;
     public float maxSpeed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(input * speed);
+        rb.linearVelocityX = (input.x * speed);
 
         if (rb.linearVelocityX > maxSpeed) 
         { 
@@ -28,6 +32,41 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(rb.linearVelocityX);
     }
 
+    private void Update()
+    {
+        if (rb.linearVelocity.x > 0)
+        { 
+            animator.SetBool("Moving", true);
+            sr.flipX = false;
+        }
+        else if (rb.linearVelocity.x < 0)
+        { 
+            animator.SetBool("Moving", true);
+            sr.flipX = true;
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+
+        if(rb.linearVelocity.y > 0)
+        {
+            animator.SetBool("Jumping", true);
+            animator.SetBool("Moving", false);
+        }
+        else if(rb.linearVelocity.y < 0)
+        {
+            animator.SetBool("Falling", true);
+            animator.SetBool("Jumping", false);
+            animator.SetBool("Moving", false);
+        }
+        else
+        {
+            animator.SetBool("Falling", false);
+            animator.SetBool("Jumping", false);
+        }  
+
+    }
     public void Move(InputAction.CallbackContext c)
     {
         input = c.ReadValue<Vector2>();
