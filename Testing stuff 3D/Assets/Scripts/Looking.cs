c2cc8daa04a8b9a1d73d4501baa49f181839ba7e
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2026-05-07 00:20:13 +0200

    Added a campfire, particles, 3D movement, 3D looking around and more

---
+++ Testing stuff 3D/Assets/Scripts/Looking.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Looking : MonoBehaviour
+{
+    Vector2 lookInput;
+    public float sensitivity;
+    float xRotation = 0f;
+
+    GameObject cam;
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void LateUpdate()
+    {
+        float mouseX = lookInput.x * sensitivity;
+        float mouseY = lookInput.y * sensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+}
---
+++ Testing stuff 3D/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    Vector2 moveInput;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDir = transform.forward * moveInput.y + transform.right * moveInput.x;
+
+        rb.linearVelocity = new Vector3(moveDir.x * moveSpeed, rb.linearVelocity.y, moveDir.z * moveSpeed);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
